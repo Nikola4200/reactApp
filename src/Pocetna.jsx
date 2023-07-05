0049d17f204222d70500db42019aed247e0099ce
Nikola Djokic <nikola.djokic@ssoe.rs>
2023-07-05 14:48:39 +0200

    Napravljena app

---
+++ src/Pocetna.jsx
@@ -0,0 +1,25 @@
+
+ 
+ 
+function Pocetna() {
+ 
+
+  return (
+    <div  className='pocetna'>
+
+       
+            
+                
+                <div className="hero-text">
+                    <h1  >Dobrodosli! </h1>
+                    <p> Najbolji kursevi stranih jezika u regionu</p> 
+               
+            </div>
+
+        
+
+    </div>
+  );
+}
+
+export default Pocetna;
